58b18eb87bec4d2b6428712f714162f80e05b74b
Avonts <vinialves022@gmail.com>
2021-04-01 15:56:02 -0300

    Add Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
-import { Title } from './styled';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styled';
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Explore repositórios no github</Title>
+        <>
+            <img src={logoImg} alt="logo" />
+            <Title>Explore repositórios no github</Title>
+            <Form>
+                <input placeholder="Digite o nome do repositório"/>
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/14189941?v=4" alt="user"/>
+                    <div>
+                        <strong>Rocketseat unform</strong>
+                        <p>Descrição do projeto</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/14189941?v=4" alt="user"/>
+                    <div>
+                        <strong>Rocketseat unform</strong>
+                        <p>Descrição do projeto</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/14189941?v=4" alt="user"/>
+                    <div>
+                        <strong>Rocketseat unform</strong>
+                        <p>Descrição do projeto</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
     );
 }
 
